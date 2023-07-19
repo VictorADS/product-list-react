f4186aaab268dd1c2074a96959824109f0db4740
vantunes <vantunes@ippon.fr>
2023-07-19 21:41:12 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Home from "./content/components/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter basename={ "/" }>
+      <div className="App">
+        <Routes>
+          <Route index element={ <Home /> } />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
